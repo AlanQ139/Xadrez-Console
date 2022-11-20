50effa9d577396a975c4d786e03fdfd0de895ab3
AlanQ139 <alanqueiroz09@hotmail.com>
2022-11-20 17:04:37 -0300

    create position class

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tabuleiro;
 
 namespace xadrez_console
 {
@@ -10,6 +11,13 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
+            Posicao P;
+            //instanciando a posição
+            P = new Posicao (3, 4);
+
+            Console.WriteLine("Posi: " + P);
+
+            Console.ReadLine();
         }
     }
 }
